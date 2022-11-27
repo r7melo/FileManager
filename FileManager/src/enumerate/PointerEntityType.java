fb8ddb51363f99b9dd53636db0a6e6acfbe66a74
r7melo <romariomelo360@gmail.com>
2022-11-27 18:20:58 -0300

    Reestruturacao das classes

--- FileManager/src/enumerate/PointerEntityType.java
+++
@@ -1,6 +0,0 @@
-package enumerate;
-
-public enum PointerEntityType {
-    Directory,
-    File
-}
--- FileManager/src/interfaces/IPointEntity.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-import javafx.scene.layout.VBox;
-
-public interface IPointEntity {
-    void openPointer(VBox displayScreen);
-}
